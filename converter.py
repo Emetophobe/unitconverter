0b5dc4bfed72fcb7e90d0beb1fc02aea96ce262b
Mike Cunningham <Mike933@gmail.com>
2023-03-13 10:23:59 -0400

    Add Unit class.
    convert() functions now use Units instead of strings.
    
    Improve function comments and add
    type hints.

--- converter.py
+++ converter.py
@@ -9,17 +9,38 @@ from pathlib import Path
 from decimal import Decimal, DecimalException
 
 
+class Unit:
+    """ A single unit of measurement. """
+
+    def __init__(self, name: str, category: str, scale: str, aliases: list[str]) -> None:
+        self.name = name
+        self.category = category
+        self.scale = scale
+        self.aliases = aliases
+
+    def __contains__(self, name) -> bool:
+        return name == self.name or name in self.aliases
+
+    def __str__(self) -> str:
+        return f'{self.scale} {self.name}, {self.category}, {self.aliases}'
+
+    def __repr__(self) -> str:
+        return f'Unit({self.scale} {self.name}, {self.category}, {self.aliases})'
+
+
 class Converter:
-    def __init__(self):
+    """ A basic unit converter. """
+
+    def __init__(self) -> None:
         self.load_units()
 
-    def convert(self, value, source, dest):
+    def convert(self, value: Decimal, source: Unit, dest: Unit) -> Decimal:
         """ Convert a number from source unit to dest unit.
 
         Args:
             value (Decimal): the decimal number to convert.
-            source (str): the source unit name.
-            dest (str): the destination unit name.
+            source (Unit): the source unit.
+            dest (Unit): the destination unit.
 
         Returns:
             Decimal: the result of the conversion.
@@ -29,51 +50,19 @@ class Converter:
         """
         value = Decimal(value)
 
-        source_category, source_name, source_scale = self.find_unit(source)
-        dest_category, dest_name, dest_scale = self.find_unit(dest)
-
-        if source_category != dest_category:
-            raise ValueError(f'Error: unit mismatch:'
-                             f' {source_name}={source_category},'
-                             f' {dest_name}={dest_category}')
-
         # Temperature is a special case
-        if source_category == 'temperature':
-            return self.convert_temperature(value, source_name, dest_name)
+        if source.category == 'temperature':
+            return self.convert_temperature(value, source, dest)
+        else:
+            return value * (Decimal(source.scale) / Decimal(dest.scale))
 
-        source_value = Decimal(source_scale)
-        dest_value = Decimal(dest_scale)
-
-        return value * (source_value / dest_value)
-
-    def find_unit(self, name):
-        """ Find a unit matching the specified name or alias.
-
-        Args:
-            name (str): the name of the unit (i.e "meters").
-
-        Returns:
-            tuple[str, str, str]: the unit category, name, and scale.
-
-        Raises:
-            ValueError: if name was not found.
-        """
-        for category, units in self.units.items():
-            for unitname, properties in units.items():
-                aliases = properties['aliases']
-                if name == unitname or name in aliases:
-                    scale = properties['scale']
-                    return (category, unitname, scale)
-
-        raise ValueError(f'Invalid unit name: {name}')
-
-    def convert_temperature(self, value, source, dest):
+    def convert_temperature(self, value: Decimal, source: Unit, dest: Unit) -> Decimal:
         """ Convert temperatures using kelvin as a baseline.
 
         Args:
             value (Decimal): the decimal value to convert.
-            source (str): the source unit name.
-            dest (str): the destination unit name.
+            source (Unit): the source unit.
+            dest (Unit): the destination unit.
 
         Returns:
             Decimal: the result of the conversion.
@@ -84,31 +73,52 @@ class Converter:
         kelvin = self.to_kelvin(value, source)
         return self.from_kelvin(kelvin, dest)
 
-    def to_kelvin(self, value, source_unit):
-        if source_unit == 'celcius':
+    def to_kelvin(self, value: Decimal, source: Unit) -> Decimal:
+        """ Convert temperature unit to kelvin. """
+        if source.name == 'celcius':
             return value + Decimal(273.15)
-        elif source_unit == 'fahrenheit':
+        elif source.name == 'fahrenheit':
             return (value + Decimal(459.67)) * Decimal(5 / 9)
-        elif source_unit == 'rankine':
+        elif source.name == 'rankine':
             return value * Decimal(5 / 9)
-        elif source_unit == 'kelvin':
+        elif source.name == 'kelvin':
             return value
         else:
-            raise ValueError(f'Invalid unit name: {source_unit}')
+            raise ValueError(f'Invalid unit name: {source.name}')
 
-    def from_kelvin(self, value, dest_unit):
-        if dest_unit == 'celcius':
+    def from_kelvin(self, value: Decimal, dest: Unit) -> Decimal:
+        """ Convert temperature unit from kelvin. """
+        if dest.name == 'celcius':
             return value - Decimal(273.15)
-        elif dest_unit == 'fahrenheit':
+        elif dest.name == 'fahrenheit':
             return value * Decimal(9 / 5) - Decimal(459.67)
-        elif dest_unit == 'rankine':
+        elif dest.name == 'rankine':
             return value * Decimal(9 / 5)
-        elif dest_unit == 'kelvin':
+        elif dest.name == 'kelvin':
             return value
         else:
-            raise ValueError(f'Invalid unit name: {dest_unit}')
+            raise ValueError(f'Invalid unit name: {dest.name}')
 
-    def load_units(self):
+    def find_unit(self, name: str) -> Unit:
+        """ Find a unit matching the specified name or alias.
+
+        Args:
+            name (str): the name of the unit (i.e "meters").
+
+        Returns:
+            Unit: the unit instance.
+
+        Raises:
+            ValueError: if name was not found.
+        """
+        for category, units in self.units.items():
+            for unitname, properties in units.items():
+                if name == unitname or name in properties['aliases']:
+                    return Unit(unitname, category, **properties)
+
+        raise ValueError(f'Invalid unit name: {name}')
+
+    def load_units(self) -> None:
         """ Load unit categories from json files. """
         self.units = {}
         for path in Path('./data/').glob('*.json'):
@@ -116,10 +126,16 @@ class Converter:
                 data = json.load(infile)
                 self.units[path.stem] = data
 
-            with open(path, 'w', encoding='utf-8') as outfile:
-                json.dump(data, outfile, indent=2)
+    def list_units(self, category: str = None) -> None:
+        """ List available units.
 
-    def list_units(self, category=None):
+        Args:
+            category (str, optional): name of the category,
+            or None to show all categories. Defaults to None.
+
+        Raises:
+            ValueError: if the category name is invalid.
+        """
         if category and category not in self.units.keys():
             raise ValueError(f'Invalid category: {category}')
 
@@ -136,19 +152,33 @@ class Converter:
                 print()
 
 
-def remove_exponent(value):
+def remove_exponent(value: Decimal) -> Decimal:
+    """ Convert exponent notation to decimal notation if possible.
+
+    Args:
+        value (Decimal): source value.
+
+    Returns:
+        Decimal: the converted value.
+    """
     if value == value.to_integral():
         return value.quantize(Decimal(1))
     else:
         return value.normalize()
 
 
-def print_error(error_msg):
+def print_error(error_msg: str, status: int = 1) -> None:
+    """ Print an error message and exit.
+
+    Args:
+        error_msg (str): the error message.
+        status (int, optional): the exit code. Defaults to 1.
+    """
     print(error_msg, file=sys.stderr)
-    sys.exit(1)
+    sys.exit(status)
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser(description='A basic unit converter.')
 
     parser.add_argument(
@@ -203,9 +233,21 @@ def main():
     if precision is not None and (precision < 0 or precision > 20):
         print_error('Error: precision must be between 0 and 20.')
 
+    # Get the source and dest units
+    try:
+        source = converter.find_unit(args.source)
+        dest = converter.find_unit(args.dest)
+    except ValueError as e:
+        print_error(e)
+
+    if source.category != dest.category:
+        raise ValueError(f'Error: unit mismatch:'
+                         f' {source.name}={source.category},'
+                         f' {dest.name}={dest.category}')
+
     # Perform the conversion
     try:
-        result = converter.convert(value, args.source, args.dest)
+        result = converter.convert(value, source, dest)
         result = remove_exponent(result)
     except ValueError as e:
         print_error(e)

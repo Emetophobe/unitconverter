b81fee54d79793d9b2c87221c2b5faa27e2089ec
Mike Cunningham <Mike933@gmail.com>
2023-03-14 09:25:26 -0400

    Add unittest for duplicate names/aliases.

---
+++ tests/test_units.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2022-2023 Mike Cunningham
+
+from tests import AbstractTestCase
+from converter import Unit
+
+
+class TestUnits(AbstractTestCase):
+
+    def test_duplicates(self) -> None:
+        """ Search for duplicate names, symbols or aliases. """
+        msg = 'Duplicate name or alias: {}'
+        aliases = {}
+
+        for unit in self.get_units():
+            self.assertNotIn(unit.name, aliases.keys(), msg.format(unit.name))
+            aliases[unit.name] = unit
+
+            for alias in unit.aliases:
+                self.assertNotIn(alias, aliases.keys(), msg.format(alias))
+                aliases[alias] = unit
+
+    def get_units(self) -> Unit:
+        """ Iterate over the unit categories and yield Units. """
+        for category, units in self.all_units.items():
+            for unitname, properties in units.items():
+                yield Unit(unitname, category, **properties)

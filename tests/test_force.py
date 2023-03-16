3aaa8303c7cd4fd98f13cdb3f6efacdb9fcc2273
Mike Cunningham <Mike933@gmail.com>
2023-03-16 17:44:57 -0400

    Add new force units.
    Units:
    
      * gram-force (gf)
      * ounce-force (ozf)
      * tonne-force (tf)
      * long ton-force (tnf)
      * short ton-force (tonf)
      * joule/meter (J/m).

--- tests/test_force.py
+++ tests/test_force.py
@@ -39,9 +39,15 @@ class TestForce(AbstractTestCase):
             "ronnanewton": "1E-27",
             "quettanewton": "1E-30",
             "dyne": "1E+5",
-            "kilogram-force": "0.1019716212977928242570092743",
-            "pound-force": "0.2248089236553391444941372081",
             "poundal": "7.233013851209894380674993482",
+            "gram-force": "101.9716212977928242570092743",
+            "ounce-force": "3.596942455035521605214703294",
+            "pound-force": "0.2248089236553391444941372081",
+            "kilogram-force": "0.1019716212977928242570092743",
+            "tonne-force": "0.0001019716212977928242570092743",
+            "long ton-force": "0.0001003611353123707512991247381",
+            "short ton-force": "0.0001124044715498552414550197067",
+            "joule/meter": "1"
         }
 
         # Test all newton conversions

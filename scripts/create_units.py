ed3d12c791da63128f832bd027b4a7f1a2809d49
Mike Cunningham <Mike933@gmail.com>
2023-03-16 12:57:10 -0400

    Update utility scripts.
    create_units.py:
    
      Positive E notation is now explicit.
    
      A default filename is now autogenerated
      based on the unit name.
    
      Add save_file() function.
    
    format_json.py:
    
      Rename to format_units.py

--- scripts/create_units.py
+++ scripts/create_units.py
@@ -18,22 +18,23 @@ METRIC_TABLE = [
     ('1e-3', 'm', 'milli'),
     ('1e-2', 'c', 'centi'),
     ('1e-1', 'd', 'deci'),
-    ('1e1', 'da', 'deca'),
-    ('1e2', 'h', 'hecto'),
-    ('1e3', 'k', 'kilo'),
-    ('1e6', 'M', 'mega'),
-    ('1e9', 'G', 'giga'),
-    ('1e12', 'T', 'tera'),
-    ('1e15', 'P', 'peta'),
-    ('1e18', 'E', 'exa'),
-    ('1e21', 'Z', 'zetta'),
-    ('1e24', 'Y', 'yotta'),
-    ('1e27', 'R', 'ronna'),
-    ('1e30', 'Q', 'quetta'),
+    ('1e+1', 'da', 'deca'),
+    ('1e+2', 'h', 'hecto'),
+    ('1e+3', 'k', 'kilo'),
+    ('1e+6', 'M', 'mega'),
+    ('1e+9', 'G', 'giga'),
+    ('1e+12', 'T', 'tera'),
+    ('1e+15', 'P', 'peta'),
+    ('1e+18', 'E', 'exa'),
+    ('1e+21', 'Z', 'zetta'),
+    ('1e+24', 'Y', 'yotta'),
+    ('1e+27', 'R', 'ronna'),
+    ('1e+30', 'Q', 'quetta'),
 ]
 
 
-def create_units(name: str, symbol: str, aliases: list[str]) -> dict:
+def create_units(name: str, symbol: str, aliases: list[str] = None) -> dict:
+    """ Create a dictionary of metric units. """
     units = {}
     for row in METRIC_TABLE:
         metric_scale, metric_symbol, metric_name = row
@@ -50,41 +51,46 @@ def create_units(name: str, symbol: str, aliases: list[str]) -> dict:
     return units
 
 
+def save_file(units: dict, filename: str, indent=2) -> None:
+    """ Save dictionary to json file. """
+    with open(filename, 'w', encoding='utf-8') as outfile:
+        json.dump(units, outfile, indent=indent)
+
+
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'name',
-        help='unit name (i.e: "amperes")')
+        help='unit name')
 
     parser.add_argument(
         'symbol',
-        help='unit symbol (i.e "A")')
+        help='unit symbol')
 
     parser.add_argument(
         'aliases',
-        help='list of aliases or shortforms (i.e "amps")',
+        help='optional list of aliases or shortforms',
         nargs='*')
 
     parser.add_argument(
         '-i', '--indent',
         help='specify a json indent level (default: %(default)s)',
         default=2,
-        type=int
-    )
+        type=int)
 
     parser.add_argument(
         '-f', '--filename',
-        help='json target file',
-        default='temp_units.json'
-    )
+        help='json target file (default: use unit name)',
+        default=None)
 
     args = parser.parse_args()
 
+    if not args.filename:
+        args.filename = args.name + '.json'
+
     units = create_units(args.name, args.symbol, args.aliases)
 
-    with open(args.filename, 'w', encoding='utf-8') as outfile:
-        json.dump(units, outfile, indent=args.indent)
-
+    save_file(units, args.filename, args.indent)
     print(f'Saved {len(units)} {args.name} units to {args.filename}')

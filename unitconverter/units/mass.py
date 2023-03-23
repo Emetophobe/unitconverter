cce6a5af2565c903354cf7988fa4c1b12dfb59b6
Mike Cunningham <Mike933@gmail.com>
2023-03-23 15:41:40 -0400

    Add comments for double checked units
    Improve accuracy of poound and imperial ton.
    
    Add kilopound (kip) for convenience.
    
    Change meter per second aliases to
    meter/s.
    
    Change meter per minute aliases to
    meter/min.

--- unitconverter/units/mass.py
+++ unitconverter/units/mass.py
@@ -2,51 +2,63 @@
 
 from unitconverter.unit import Unit
 
-
-# SI base unit for mass is the kilogram
-# I use gram for convenience
+# SI unit for mass is kilograms (kg) but I use gram (g) for simplicity.
 gram = Unit(
 	name='gram',
 	symbols='g',
 	aliases='grams',
 	factor='1')
 
+# 1 ounce is 28.34952 grams
 ounce = Unit(
 	name='ounce',
 	symbols='oz',
-	aliases='ounces',
+	aliases=['ounces', 'imperial ounces', 'imperial ounce'],
 	factor='28.34952')
 
+# 1 pound is 453.59237 grams
 pound = Unit(
 	name='pound',
 	symbols='lbs',
-	aliases='pounds',
-	factor='453.592')
+	aliases=['pounds', 'imperial pounds', 'imperial pound'],
+	factor='453.59237')
 
+# 1 kilopound (kip) = 1000 lbs or 453592.37 grams
+kilopound = Unit(
+	name='kilopound',
+	symbols='kip',
+	aliases='kilopounds',
+	factor='453592.37')
+
+# 1 stone = 14 lbs or 6350.29 grams
 stone = Unit(
 	name='stone',
 	symbols='st',
-	aliases='stones',
+	aliases=['stones', 'imperial stone'],
 	factor='6350.29')
 
+# Metric ton is 1E+6 grams (1,000,000 g)
+tonne = Unit(
+	name='tonne',
+	symbols='t',
+	aliases=['tonnes', 'metric tons', 'metric ton', 'tons', 'ton'],
+	factor='1E+6')
+
+# 1 imperial ton = 1016046.91 grams
 imperial_ton = Unit(
 	name='imperial ton',
 	symbols='LT',
 	aliases=['imperial tons', 'long tons', 'long ton'],
-	factor='1016046.9')
+	factor='1016046.91')
 
+# 1 US ton = 907184.74 grams
 us_ton = Unit(
 	name='US ton',
 	symbols='ST',
 	aliases=['US tons', 'short tons', 'short ton'],
 	factor='907184.74')
 
-tonne = Unit(
-	name='tonne',
-	symbols='t',
-	aliases=['tonnes', 'metric tons', 'metric ton'],
-	factor='1E+6')
-
+# 1 carat = 0.2 grams
 carat = Unit(
 	name='carat',
 	symbols=['car', 'ct'],
--- unitconverter/units/speed.py
+++ unitconverter/units/speed.py
@@ -3,24 +3,28 @@
 from unitconverter.unit import Unit
 
 
+# 1 meter/minute (m/s) is the base unit
 meter_per_second = Unit(
 	name='meter per second',
 	symbols='m/s',
-	aliases=['meters per second', 'metres per second', 'metre per second'],
+	aliases=['meter/s', 'meters/s', 'metres/s', 'metre/s'],
 	factor='1')
 
+# 1 meter/minute = 1/60 = 0.0166666667 m/s
 meter_per_minute = Unit(
 	name='meter per minute',
 	symbols='m/min',
-	aliases=['meters per minute', 'metres per minute', 'metre per minute'],
+	aliases=['meter/min', 'meters/min', 'metres/min', 'metre/min'],
 	factor='0.0166666667')
 
+# 1 meter/hour = 1/3600 = 0.0002777778 m/s
 meter_per_hour = Unit(
 	name='meter per hour',
 	symbols='m/hr',
 	aliases=['meters per hour', 'metres per hour', 'metre per hour'],
 	factor='0.0002777778')
 
+# 1 kph = 1000/3600 = 0.2777777778 m/s
 kilometer_per_hour = Unit(
 	name='kilometer per hour',
 	symbols=['kph', 'km/hr'],
@@ -28,49 +32,56 @@ kilometer_per_hour = Unit(
 	factor='0.2777777778',
 	scaling='none')
 
+# 1 mph = 0.44704 m/s
 mile_per_hour = Unit(
 	name='mile per hour',
 	symbols=['mph', 'mi/hr'],
 	aliases='miles per hour',
 	factor='0.44704')
 
+# 1 inch/second = 0.0254 m/s
 inch_per_second = Unit(
 	name='inch per second',
 	symbols=['in/s', 'in/sec'],
 	aliases='inches per second',
 	factor='0.0254')
 
+# 1 inch/minute = 0.000423333 m/s
 inch_per_minute = Unit(
 	name='inch per minute',
 	symbols='in/min',
 	aliases='inches per minute',
 	factor='0.000423333')
 
+# 1 inch/hour = 7.05556e-6 m/s
 inch_per_hour = Unit(
 	name='inch per hour',
 	symbols='in/hr',
 	aliases='inches per hour',
 	factor='7.05556e-6')
 
+# 1 foot/second = 0.3048 m/s
 foot_per_second = Unit(
 	name='foot per second',
 	symbols=['ft/s', 'ft/sec'],
 	aliases='feet per second',
 	factor='0.3048')
 
+# 1 foot/minute = 0.00508 m/s
 foot_per_minute = Unit(
 	name='foot per minute',
 	symbols='ft/min',
 	aliases='feet per minute',
 	factor='0.00508')
 
+# 1 foot/hour = 0.0000846667 m/s
 foot_per_hour = Unit(
 	name='foot per hour',
 	symbols='ft/hr',
 	aliases='feet per hour',
 	factor='0.0000846667')
 
-# The knot is equal to one nautical mile per hour (1.852 km/h or 0.514 m/s)
+# The knot is equal to one nautical mile per hour (~0.514 m/s)
 knot = Unit(
 	name='knot',
 	symbols='',

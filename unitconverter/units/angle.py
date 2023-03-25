6ecc11b7841f48103dce1589be7d1b22bd5a4557
Mike Cunningham <Mike933@gmail.com>
2023-03-24 22:57:58 -0400

    Update angle units
    Move factor to 2nd argument.
    
    Disable prefix scaling for spat.

--- unitconverter/units/angle.py
+++ unitconverter/units/angle.py
@@ -10,26 +10,27 @@ from unitconverter.unit import Unit
 # 1 steradian =  (180/pi)^2 or roughly 3282.8 square degrees
 steradian = Unit(
     name='steradian',
+    factor='1',
     symbols='sr',
     aliases='steradians',
-    category='solid angle',
-    factor='1')
+    category='solid angle')
 
 # 1 square degree = (pi*180)^2 or roughly 0.000304617419786 steradians
 square_degree = Unit(
     name='square degree',
+    factor='3.04617E-4',
     symbols=['deg^2', 'deg2'],
     aliases='square degrees',
     category='solid angle',
-    prefix_scaling='none',
-    factor='3.04617E-4')
+    prefix_scaling='none')
 
 spat = Unit(
     name='spat',
+    factor='12.5663706144',
     symbols='sp',
     aliases='spats',
     category='solid angle',
-    factor='12.5663706144')
+    prefix_scaling='none')
 
 # Plane angle
 
@@ -37,25 +38,25 @@ spat = Unit(
 # 1 radian = 180/pi degrees or roughly 57.295779513 degrees
 radian = Unit(
     name='radian',
+    factor='1',
     symbols='rad',
     aliases='radians',
-    category='plane angle',
-    factor='1')
+    category='plane angle')
 
 # 1 degree = pi/180 radians or roughly 0.0174532925 radians
 degree = Unit(
     name='degree',
-    symbols='°',
+    factor='0.0174532925',
+    symbols=['°', 'deg'],
     aliases='degrees',
     category='plane angle',
-    prefix_scaling='none',
-    factor='0.0174532925')
+    prefix_scaling='none')
 
 # 1 gradian = pi/200 radians or roughly 0.015708 radians
 gradian = Unit(
     name='gradian',
+    factor='0.015708',
     symbols='gon',
     aliases=['gradians', 'grade', 'grad'],
     category='plane angle',
-    prefix_scaling='none',
-    factor='0.015708')
+    prefix_scaling='none')

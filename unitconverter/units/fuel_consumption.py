a293177d235624c8e75a60799f14c02cec735d48
Mike Cunningham <Mike933@gmail.com>
2023-03-24 21:44:43 -0400

    Update prefix settings
    Prefix first word for the following units:
    
      litre per metre
      metre per litre
      litre pre 100km
      metre per UK gallon
      metre per US gallon
    
    Disable prefix scaling for the following units:
    
      UK gallon per metre
      US gallon per metre
      mile per UK gallon
      mile per US gallon

--- unitconverter/units/fuel_consumption.py
+++ unitconverter/units/fuel_consumption.py
@@ -8,69 +8,69 @@ litre_per_metre = Unit(
     name='litre per metre',
     symbols=['L/m', 'l/m'],
     aliases=['litres per metre', 'liters/metre', 'liter/metre'],
-	prefix_index=0,
-    factor='1')
+    factor='1',
+    prefix_index=0)
 
 # 1 metre/litre equals 1 litre/metre
 metre_per_litre = Unit(
     name='metre per litre',
     symbols=['m/L', 'm/l'],
     aliases=['metres per litre', 'metres/liter', 'metre/liter'],
-	prefix_index=0,
-    factor='1')
+    factor='1',
+    prefix_index=0)
 
 # 1 litre/100km = 100,000 metres/litre
 litre_per_100km = Unit(
     name='litre per 100km',
     symbols=['L/100km', 'l/100km'],
     aliases=['litres per 100km', 'liters/100km', 'liter/100km'],
-	prefix_index=0,
-    factor='100000')
+    factor='100000',
+    prefix_index=0)
 
 # 1 metre/UK gallon = 0.2199687986 metres/litre
 metre_per_uk_gallon = Unit(
     name='metre per UK gallon',
     symbols='m/ukgal',
     aliases=['metres per UK gallon', 'metre/ukgallon', 'metre/ukgal'],
-	prefix_index=0,
-    factor='0.2199687986')
+    factor='0.2199687986',
+    prefix_index=0)
 
 # 1 metre/US gallon = 0.2641720524 metres/litre
 metre_per_us_gallon = Unit(
     name='metre per US gallon',
     symbols='m/usgal',
     aliases=['metres per US gallon', 'metre/usgallon', 'metre/usgal'],
-	prefix_index=0,
-    factor='0.2641720524')
+    factor='0.2641720524',
+    prefix_index=0)
 
 # 1 UK gallon/mile = 354.00619 metres/litre
 uk_gallon_per_mile = Unit(
     name='UK gallon per mile',
-	factor='354.00619',
+    factor='354.00619',
     symbols=['ukgal/mi', 'gal/mi (UK)'],
     aliases=['UK gallons per mile', 'ukgallon/mile', 'ukgal/mile'],
-	prefix_index=0)
+    prefix_scaling='none')
 
 # 1 UK mile/gallon = 354.00619 metres/litre
 uk_mile_per_gallon = Unit(
     name='mile per UK gallon',
-	factor='354.00619',
+    factor='354.00619',
     symbols= ['mpg (UK)', 'mi/gal (UK)'],
     aliases=['miles per UK gallon', 'miles/ukgallon', 'miles/ukgal'],
-	prefix_index=0)
+    prefix_scaling='none')
 
 # 1 US gallon/mile = 425.1437075 metres/litre
 us_gallon_per_mile = Unit(
     name='US gallon per mile',
-	factor='425.1437075',
+    factor='425.1437075',
     symbols=['usgal/mi', 'gal/mi (US)'],
     aliases=['US gallons per mile', 'usgallon/mile', 'usgal/mile'],
-	prefix_index=0)
+    prefix_scaling='none')
 
 # 1 mile/US gallon = 425.1437075 metres/litre
 us_mile_per_gallon = Unit(
     name='mile per US gallon',
-	factor='425.1437075',
+    factor='425.1437075',
     symbols = ['mpg (US)', 'mi/gal (US)'],
     aliases=['miles per US gallon', 'miles/usgallon', 'miles/usgal'],
-	prefix_index=0)
+    prefix_scaling='none')

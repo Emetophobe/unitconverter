6ecc0974791b5848480d44bcd795da29f61c870d
Mike Cunningham <Mike933@gmail.com>
2023-03-25 08:45:08 -0400

    Add cycle/second and cycle/minute for convenience
    1 cycle/second is equivalent to 1 hertz
    1 cycle/minute is equivalent to 1/60th of a hertz

--- unitconverter/units/frequency.py
+++ unitconverter/units/frequency.py
@@ -13,9 +13,26 @@ hertz = Unit(
     prefix_scaling='si')
 
 
-# 1 rpm is roughly 0.016666667 hertz (60 rpm is equal to 1 hertz)
+# 1 cycle per second is equivalent to 1 hertz
+cycle_per_second = Unit(
+    name='cycle per second',
+    factor='1',
+    symbols=['cps', 'CPS'],
+    aliases=['cycles per second', 'cycles/second', 'cycle/second', 'cycles/sec',
+             'cycle/sec'])
+
+# 1 cycle per minute is 1/60 hertz or roughly 0.016666667 hertz
+cycle_per_minute = Unit(
+    name='cycle per minute',
+    factor=Decimal('1') / Decimal('60'),
+    symbols=['cpm', 'CPM'],
+    aliases=['cycles per minute', 'cycles/minute', 'cycle/minute', 'cycles/min',
+             'cycle/min'])
+
+# Add rpm for convenience. 1 rpm is the same as 1 cycle/minute
 rpm = Unit(
     name='revolution per minute',
     factor=Decimal('1') / Decimal('60'),
     symbols=['rpm', 'RPM'],
-    aliases='revolutions per minute')
+    aliases=['revolutions per minute', 'revolutions/minute', 'revolution/minute',
+             'revolutions/min', 'revolution/min', 'revs/min', 'rev/min'])

d3a83c518faae3e3fdab1d09c3f6c8be3b14503f
Mike Cunningham <Mike933@gmail.com>
2023-03-26 07:00:44 -0400

    Add combinations() and several constants
    Add the following contants:
    
      METRE_NAMES
      LITRE_NAMES
      PER_SECOND
      PER_HOUR
      PER_MINUTE
      PER_METRE
      PER_LITRE
      PER_GALLON
      PER_USGALLON

---
+++ unitconverter/utils.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2022-2023 Mike Cunningham
+
+
+def combinations(prefixes, suffixes, excludes=None):
+    """ Create prefix + suffix combinations from two string lists. """
+    if not isinstance(prefixes, (list, tuple)):
+        raise TypeError(f'{prefixes!r} is not a list or tuple.')
+
+    if not isinstance(suffixes, (list, tuple)):
+        raise TypeError(f'{suffixes!r} is not a list or tuple.')
+
+    excludes = excludes or []
+    names = []
+    for prefix in prefixes:
+        for suffix in suffixes:
+            merged = prefix + suffix
+            if merged not in names and merged not in excludes:
+                names.append(merged)
+
+    return names
+
+
+# Tuples of commonly used prefixes and suffixes
+METRE_NAMES = ('metre', 'metres', 'meter', 'meters')
+LITRE_NAMES = ('litre', 'litres', 'liter', 'litres')
+
+# Leading space before 'per' is intentional
+PER_SECOND = (' per second', '/second', '/sec', '/s')
+PER_MINUTE = (' per minute', '/minute', '/min')
+PER_HOUR = (' per hour', '/hour', '/hr')
+PER_METRE = combinations([' per ', '/'], ['metre', 'meter'])
+PER_LITRE = combinations([' per ', '/'], ['litre', 'liter'])
+PER_GALLON = combinations([' per ', '/'], ['gallon', 'gal'])
+PER_USGALLON = combinations([' per ', '/'], ['usgallon', 'usgal'])

e9864d997aa7f123b92f81812ee2c9e0bef8080c
Mike Cunningham <Mike933@gmail.com>
2023-03-26 07:19:41 -0400

    Add watt per square metre aliases

--- unitconverter/units/illuminance.py
+++ unitconverter/units/illuminance.py
@@ -37,7 +37,7 @@ watt_per_square_metre = Unit(
     name='watt per square metre',
     factor='683',
     symbols=['W/m^2', 'W/m2'],
-    aliases=['watts per square metre', 'watt/m^2', 'watt/m2'],
+    aliases=['watts per square metre', 'watts/m^2', 'watts/m2', 'watt/m^2', 'watt/m2'],
     prefix_scaling='si',
     prefix_index=0)
 

f4df7222d0f80e6ee8b351667f88baab99117f18
Mike Cunningham <Mike933@gmail.com>
2023-03-26 12:02:10 -0400

    Add coulomb/volt, abfarad, and statfarad
    Remove old Radiation category

--- unitconverter/units/electrical_capacitance.py
+++ unitconverter/units/electrical_capacitance.py
@@ -14,3 +14,26 @@ farad = ElectricalCapacitance(
     symbols='F',
     aliases='farads',
     prefix_scaling='si')
+
+# 1 coulomb/volt is equivalent to 1 farad
+coulomb_per_volt = ElectricalCapacitance(
+    name='coulomb per volt',
+    factor='1',
+    symbols='C/V',
+    aliases=['coulombs per volt', 'coulomb/volt', 'coulombs/volt'],
+    prefix_scaling='si',
+    prefix_index=0)
+
+# 1 abfarad = 1E+9 farads
+abfarad = ElectricalCapacitance(
+    name='abfarad',
+    factor='1E+9',
+    symbols='abF',
+    aliases='abfarads')
+
+# 1 statfarad = 1.112650056E-12 farads
+statfarad = ElectricalCapacitance(
+    name='statfarad',
+    factor='1.112650056E-12',
+    symbols='stF',
+    aliases='statfarads')

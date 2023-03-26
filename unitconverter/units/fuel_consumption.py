2e6c35c067665dd4dd2b140674fe3aa1062d0def
Mike Cunningham <Mike933@gmail.com>
2023-03-26 07:09:03 -0400

    Use combinations() to generate aliases
    Rename PER_GALLON to PER_UKGALLON.

--- unitconverter/units/fuel_consumption.py
+++ unitconverter/units/fuel_consumption.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2022-2023 Mike Cunningham
 
 from unitconverter.unit import Unit
+from unitconverter.utils import (combinations, METRE_NAMES, LITRE_NAMES, PER_METRE,
+                                 PER_LITRE, PER_UKGALLON, PER_USGALLON)
 
 
 # litre per metre is the base unit
@@ -8,8 +10,7 @@ litre_per_metre = Unit(
     name='litre per metre',
     factor='1',
     symbols=['L/m', 'l/m'],
-    aliases=['litres per metre', 'liter per minute', 'litres per minute',
-             'litre/metre', 'litres/metre', 'liter/meter', 'liters/meter'],
+    aliases=combinations(LITRE_NAMES, PER_METRE),
     prefix_scaling='si',
     prefix_index=0)
 
@@ -18,8 +19,7 @@ metre_per_litre = Unit(
     name='metre per litre',
     factor='1',
     symbols=['m/L', 'm/l'],
-    aliases=['metres per litre', 'meters per liter', 'meter per liter', 'metre/litre',
-             'metres/litre', 'meter/liter', 'meters/liter'],
+    aliases=combinations(METRE_NAMES, PER_LITRE),
     prefix_scaling='si',
     prefix_index=0)
 
@@ -28,60 +28,54 @@ litre_per_100km = Unit(
     name='litre per 100km',
     factor='0.00001',
     symbols=['L/100km', 'l/100km'],
-    aliases=['litres per 100km', 'liters per 100km', 'liter per 100km',
-             'litre/100km', 'litres/100km', 'liter/100km', 'liters/100km'],
+    aliases=combinations(LITRE_NAMES, [' per 100km', '/100km']),
     prefix_scaling='si',
     prefix_index=0)
 
 
 # UK gallons
 
-# 1 foot per ukgallon is roughly 0.067046626881562 litres/metre
+
+# 1 foot per uk gallon is roughly 0.067046626881562 litres/metre
 foot_per_ukgallon = Unit(
     name='foot per gallon',
     factor='0.067046626881562',
     symbols=['ft/gal', 'ft/ukgal'],
-    aliases=['feet per gallon', 'foot/gallon', 'feet/gallon', 'foot/gal', 'feet/gal'])
+    aliases=combinations(['foot', 'feet'], PER_UKGALLON))
 
-# 1 metre per ukgallon is roughly 0.2199687986 litre/metre
+# 1 metre per uk gallon is roughly 0.2199687986 litre/metre
 metre_per_ukgallon = Unit(
     name='metre per gallon',
     factor='0.2199687986',
     symbols=['m/gal', 'm/ukgal'],
-    aliases=['metres per gallon', 'meter per gallon', 'meters per gallon',
-             'metre/gallon', 'metres/gallon', 'meter/gallon', 'meters/gallon',
-             'metre/gal', 'metres/gal', 'meter/gal', 'meters/gal'],
+    aliases=combinations(METRE_NAMES, PER_UKGALLON),
     prefix_scaling='si',
     prefix_index=0)
 
-# 1 mile per ukgallon is roughly 0.0028248108789117 litre/metre
+# 1 mile per uk gallon is roughly 0.0028248108789117 litre/metre
 mile_per_ukgallon = Unit(
     name='mile per gallon',
     factor='0.0028248108789117',
     symbols=['mpg', 'ukmpg', 'mi/gal'],
-    aliases=['miles per gallon', 'mile/gallon', 'miles/gallon',
-             'miles/gal', 'mile/gal'])
+    aliases=combinations(['mile', 'miles'], PER_UKGALLON))
 
 
 # US gallons
 
 
-# 1 foot per usgallon is roughly 0.080519999871169 litres/metre
+# 1 foot per us gallon is roughly 0.080519999871169 litres/metre
 foot_per_usgallon = Unit(
     name='foot per usgallon',
     factor='0.080519999871169',
     symbols='ft/usgal',
-    aliases=['feet per usgallon', 'foot/usgallon', 'feet/usgallon',
-             'foot/usgal', 'feet/usgal'])
+    aliases=combinations(['foot', 'feet'], PER_USGALLON))
 
-# 1 metre per usgallon is roughly 0.2641720524 litre/metre
+# 1 metre per us gallon is roughly 0.2641720524 litre/metre
 metre_per_usgallon = Unit(
     name='metre per usgallon',
     factor='0.2641720524',
     symbols='m/usgal',
-    aliases=['metres per usgallon', 'metre/usgallon', 'metres/usgallon',
-             'meters/usgallon', 'meter/usgallon', 'metre/usgal',
-             'metres/usgal', 'meter/usgal', 'meters/usgal'],
+    aliases=combinations(METRE_NAMES, PER_USGALLON),
     prefix_scaling='si',
     prefix_index=0)
 
@@ -90,5 +84,4 @@ mile_per_usgallon = Unit(
     name='mile per usgallon',
     factor='0.0023521442146661',
     symbols=['usmpg', 'mi/usgal'],
-    aliases=['miles per usgallon', 'mile/usgallon', 'miles/usgallon',
-             'miles/usgal', 'mile/usgal'])
+    aliases=combinations(['mile', 'miles'], PER_USGALLON))

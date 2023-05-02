2f1db3d02dab9f4b94fabf97e3d1bbfa9e75e335
Mike Cunningham <Mike933@gmail.com>
2023-05-01 21:18:40 -0400

    Add tests for get_dimensions() and get_category()

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -3,9 +3,10 @@
 
 import unittest
 
-from unitconverter.exceptions import UnitError, DefinitionError
-from unitconverter.registry import REGISTRY as registry
 from unitconverter.definitions import UnitDefinition
+from unitconverter.exceptions import DefinitionError, UnitError
+from unitconverter.registry import REGISTRY as registry
+from unitconverter.unit import Unit
 
 
 class TestRegistry(unittest.TestCase):
@@ -98,6 +99,29 @@ class TestRegistry(unittest.TestCase):
 
         with self.assertRaises(DefinitionError):
             registry.get_definition(None)
+
+    def test_get_dimensions(self) -> None:
+        """ Test get_dimensions() method. """
+        dimensions = registry.get_dimensions()
+
+        self.assertIsInstance(dimensions, dict)
+        self.assertGreater(len(dimensions), 0)
+
+        for dimen, category in dimensions.items():
+            self.assertIsInstance(dimen, tuple)
+            self.assertIsInstance(category, str)
+
+    def test_get_category(self) -> None:
+        """ Test get_category() method. """
+        unit = Unit(1, {'metre': 1}, {'length': 1, 'time': -1})
+        speed = registry.get_category(unit)
+        self.assertEqual(speed, 'speed')
+
+        dimensionless = registry.get_category(Unit(1))
+        self.assertEqual(dimensionless, 'dimensionless')
+
+        with self.assertRaises(UnitError):
+            registry.get_category(None)
 
     def test_load_units(self) -> None:
         """ Test _load_units() method. """
